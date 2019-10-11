0d17f666efc7b140c2987c8ad05ec5de4f0ede7b
Hua <lhcn@qq.com>
2019-10-11 14:50:37 +0800

    t

---
+++ Project02/src/com/lh/t1/Demo1.java
@@ -0,0 +1,10 @@
+package com.lh.t1;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Hi1");
+        System.out.println("Hi2");
+        System.out.println("Hi3");
+
+    }
+}
