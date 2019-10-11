f12a6d22089524763925649498c839c642382d30
Bruce.Lee <lhcn@qq.com>
2019-10-11 21:11:11 +0800

    dev edit 3

--- Project02/src/com/lh/t1/Demo2.java
+++ Project02/src/com/lh/t1/Demo2.java
@@ -3,11 +3,11 @@ package com.lh.t1;
 public class Demo2 {
     public static void main(String[] args) {
         String str;
-        str = "ABCD";
+        str = "ABCDE";
         printString(str);
 
     }
-    public static void printString(String str){
+    private static void printString(String str){
         System.out.println(str);
     }
 }
