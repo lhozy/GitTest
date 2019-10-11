1afcee7de58a7afd181868bce812e7e0124c8028
Bruce.Lee <lhcn@qq.com>
2019-10-11 20:47:01 +0800

    dev1 添加demo2

---
+++ Project02/src/com/lh/t1/Demo2.java
@@ -0,0 +1,13 @@
+package com.lh.t1;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        String str;
+        str = "ABC";
+        printString(str);
+
+    }
+    public static void printString(String str){
+        System.out.println(str);
+    }
+}
